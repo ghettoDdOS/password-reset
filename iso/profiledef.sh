219fa3650555e6ca0089c6e42ddc47eafff8b7a7
penkhasoveg <pen.egor2002@gmail.com>
2023-04-28 17:28:03 +0300

    feat: iso autologin

--- iso/profiledef.sh
+++ iso/profiledef.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2034
 
 iso_name="password-reset"
-iso_label="PASSWORD_RESET_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_label="PASSWORD_RESET$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="ghettoDdOS <https://github.com/ghettoDdOS/>"
 iso_application="PasswordReset"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
@@ -16,6 +16,7 @@ pacman_conf="pacman.conf"
 airootfs_image_type="erofs"
 airootfs_image_tool_options=('-zlzma,9' -E ztailpacking)
 file_permissions=(
-  ["/etc/shadow"]="0:0:400"
-  ["/usr/bin/password_reset"]="0:0:755"
+  ["/etc/shadow"]="0:0:0400"
+  ["/etc/gshadow"]="0:0:0400"
+  ["/usr/local/bin/password_reset"]="0:0:755"
 )
