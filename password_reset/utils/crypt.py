078b38d69e2481ea6d3ad0af618df9b0d0e260a0
ghettoDdOS <pen.egor2002@gmail.com>
2023-04-28 21:41:59 +0300

    fix: shadow password escape

--- password_reset/utils/crypt.py
+++ password_reset/utils/crypt.py
@@ -2,6 +2,15 @@ import subprocess
 
 
 def shadow_password(password: str) -> str:
-    return subprocess.check_output(("openssl", "passwd", "-6", password)).decode(
-        "utf-8"
+    return (
+        subprocess.check_output(
+            (
+                "openssl",
+                "passwd",
+                "-6",
+                password,
+            )
+        )
+        .decode("utf-8")
+        .strip()
     )
